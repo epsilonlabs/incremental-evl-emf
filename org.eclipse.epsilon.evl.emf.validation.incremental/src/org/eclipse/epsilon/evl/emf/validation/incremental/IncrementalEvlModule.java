104a157d2ec3e74555191100da7991df0accc7bb
OwenR-York <owen.reynolds@york.ac.uk>
2023-02-09 11:38:56 +0000

    First attempt to inspect on validation execution

--- org.eclipse.epsilon.evl.emf.validation.incremental/src/org/eclipse/epsilon/evl/emf/validation/incremental/IncrementalEvlModule.java
+++ org.eclipse.epsilon.evl.emf.validation.incremental/src/org/eclipse/epsilon/evl/emf/validation/incremental/IncrementalEvlModule.java
@@ -1,8 +1,13 @@
 package org.eclipse.epsilon.evl.emf.validation.incremental;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.epsilon.common.module.ModuleElement;
 import org.eclipse.epsilon.common.parse.AST;
 import org.eclipse.epsilon.eol.exceptions.EolRuntimeException;
@@ -14,50 +19,85 @@ import org.eclipse.epsilon.evl.dom.Constraint;
 import org.eclipse.epsilon.evl.execute.UnsatisfiedConstraint;
 
 public class IncrementalEvlModule extends EvlModule {
-	
-	protected ConstraintPropertyAccessRecorder propertyAccessRecorder = new ConstraintPropertyAccessRecorder();
-	protected IncrementalEvlTrace trace = new IncrementalEvlTrace();
-	
-	@Override
-	public ModuleElement adapt(AST cst, ModuleElement parentAst) {
-		ModuleElement moduleElement = super.adapt(cst, parentAst);
-		if (moduleElement instanceof Constraint) {
-			
-			
-			return new Constraint() {
-				public Optional<UnsatisfiedConstraint> execute(IEolContext context_, Object self) throws EolRuntimeException {
-					
-					// We could check here if the last execution -- return the result of the last test
-					
-					// else run the test					
-					propertyAccessRecorder.setExecution(new ConstraintExecution(this, self));
-					return super.execute(context_, self);
-				};
-			};
-		}
-		return moduleElement;
-	}
-	
-	@Override
-	public Set<UnsatisfiedConstraint> execute() throws EolRuntimeException {
-		propertyAccessRecorder.startRecording();
-		getContext().getExecutorFactory().addExecutionListener(new PropertyAccessExecutionListener(propertyAccessRecorder));
-		
-		Set<UnsatisfiedConstraint> unsatisfiedConstraints = super.execute();  // persist this in the instance?	
-		
-		for (IPropertyAccess propertyAccess : propertyAccessRecorder.getPropertyAccesses().all()) {
-			trace.addPropertyAccess((ConstraintPropertyAccess) propertyAccess);
-		}
-		
-		return unsatisfiedConstraints;
-	}
-	
-	public ConstraintPropertyAccessRecorder getPropertyAccessRecorder() {
-		return propertyAccessRecorder;
-	}
-	
-	public IncrementalEvlTrace getTrace() {
-		return trace;
-	}
-	
+
+    public List<Notification> notifications;
+    private Set<UnsatisfiedConstraint> unsatisfiedConstraints;
+    private IncrementalEvlTrace lastTrace;
+
+    public IncrementalEvlModule() {
+        System.out.println(" [i] IncrementalEclModel constructor");
+    }
+
+    public IncrementalEvlModule(List<Notification> notifications, Set<UnsatisfiedConstraint> unsatisfiedConstraints, IncrementalEvlTrace lastTrace) {
+        System.out.println(" [i] IncrementalEclModel constructor with settings");
+        this.notifications = notifications;
+        this.unsatisfiedConstraints = unsatisfiedConstraints;
+        this.lastTrace = trace;
+    }
+
+    protected ConstraintPropertyAccessRecorder propertyAccessRecorder = new ConstraintPropertyAccessRecorder();
+    protected IncrementalEvlTrace trace = new IncrementalEvlTrace();
+
+    @Override
+    public ModuleElement adapt(AST cst, ModuleElement parentAst) {
+
+        ModuleElement moduleElement = super.adapt(cst, parentAst);
+
+        // OK this gets tricky here as we seem to determine what things are and create new instances and return them
+        if (moduleElement instanceof Constraint) {
+            Constraint tempContraint = (Constraint) moduleElement;
+            System.out.println("what this? hashcode : " + tempContraint);
+
+            return new Constraint() {
+                public Optional<UnsatisfiedConstraint> execute(IEolContext context_, Object self) throws EolRuntimeException {
+
+                    // A context and a self, what is a self?
+                    System.out.println("self is a : " + self.getClass());
+                    EClass model = (EClass) self;
+                    System.out.println("self is a model element >> HashCode : " + model.hashCode());
+
+                    // Is model element in the notification list?
+                    if (null != notifications){
+                        for (Notification n: notifications) {
+                            EStructuralFeature feature = (EStructuralFeature) n.getFeature();
+                            System.out.println(" !!! - I GOT THAT THING YOU SENT ME - !!! HashCode: " + feature.getName() );
+                        }
+                    }
+
+
+                    // We could check here if the last execution -- return the result of the last test
+                    // Is there a notification? Is it on the last Trace? Is it listed as an unsatisfied constrain?
+                    // else run the test			1
+                    propertyAccessRecorder.setExecution(new ConstraintExecution(this, self));
+                    return super.execute(context_, self);
+                }
+
+
+            };
+        }
+        return moduleElement;
+    }
+
+    @Override
+    public Set<UnsatisfiedConstraint> execute() throws EolRuntimeException {
+        propertyAccessRecorder.startRecording();
+        getContext().getExecutorFactory().addExecutionListener(new PropertyAccessExecutionListener(propertyAccessRecorder));
+
+        Set<UnsatisfiedConstraint> unsatisfiedConstraints = super.execute();  // persist this in the instance?
+
+        for (IPropertyAccess propertyAccess : propertyAccessRecorder.getPropertyAccesses().all()) {
+            trace.addPropertyAccess((ConstraintPropertyAccess) propertyAccess);
+        }
+
+        return unsatisfiedConstraints;
+    }
+
+    public ConstraintPropertyAccessRecorder getPropertyAccessRecorder() {
+        return propertyAccessRecorder;
+    }
+
+    public IncrementalEvlTrace getTrace() {
+        return trace;
+    }
+
 }
--- org.eclipse.epsilon.evl.emf.validation.incremental/src/org/eclipse/epsilon/evl/emf/validation/incremental/IncrementalEvlValidatorAdapter.java
+++ org.eclipse.epsilon.evl.emf.validation.incremental/src/org/eclipse/epsilon/evl/emf/validation/incremental/IncrementalEvlValidatorAdapter.java
@@ -1,6 +1,5 @@
 package org.eclipse.epsilon.evl.emf.validation.incremental;
 
-import java.net.URI;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -12,119 +11,118 @@ import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.util.EContentAdapter;
 import org.eclipse.epsilon.emc.emf.InMemoryEmfModel;
-import org.eclipse.epsilon.eol.models.IModel;
 import org.eclipse.epsilon.evl.dom.Constraint;
 import org.eclipse.epsilon.evl.execute.UnsatisfiedConstraint;
 
 
-
-
 public class IncrementalEvlValidatorAdapter extends EContentAdapter {
-	
-	private String lastValidation = "";  
-	
-	//private ??? lastConstraints = null;
-	private IncrementalEvlTrace lastTrace = null;
-	private Set<UnsatisfiedConstraint> unsatisfiedConstraints;
-	
-	protected IncrementalEvlValidator validator = null;
-	protected List<Notification> notifications = new ArrayList<>();
-	
-	public IncrementalEvlValidatorAdapter(IncrementalEvlValidator validator) {
-		this.validator = validator;
-	}
-	
-	public void revalidate(ResourceSet resourceSet) throws Exception {
-		System.out.println("\n [!] IncrementalEvlValidatorAdapter.revalidate() called: " + resourceSet +"\n");
-		
-		validate(resourceSet);
-		
-		notifications.clear();
-		
-	}
-	
-	public void validate(ResourceSet resourceSet) throws Exception {
-		System.out.print("\n [!] IncrementalEvlValidatorAdapter.validate() called\n");
-		
-		// Model (root element)
-		InMemoryEmfModel model = new InMemoryEmfModel(resourceSet.getResources().get(0));
-		model.setConcurrent(true);
-		System.out.println("Model name : '" + model.getName() + "' hashCode: " + model.hashCode());
-		
-		// All Model elements
-		System.out.println("Model elements :");
-		Collection<EObject> elements = model.allContents();
-		for(EObject e : elements) {
-			System.out.println("hashCode: " + e.hashCode() + " object: " + e.toString());
-		}
-		
-		
-		// Module for doing evaluations
-		IncrementalEvlModule module = new IncrementalEvlModule();
-		module.parse(validator.getConstraints());	// constraints ArrayList<E>[]	
-		module.getContext().getModelRepository().addModel(model);
-		
-		
-		System.out.println("\nConstraints to Execute : ");
-		List<Constraint> constraintsToExecute = module.getConstraints();
-		int i =0;
-		for(Constraint c : constraintsToExecute) {
-			i++;
-			System.out.println(i + ", " + c.getName());
-		}
-		
 
-		
-		
-		//Set<UnsatisfiedConstraint> unsatisfiedConstraints = module.execute();
-		System.out.println("\n [!] ...Executing validation...\n");
-		unsatisfiedConstraints = module.execute();
-		
-		
-		lastTrace = module.getTrace();
-		System.out.println("\n [!] Review trace (constraintPropertyAccess Objects) in EvlModule:\n" + lastTrace.propertyAccesses.toString());
-		if(null != lastTrace)
-		{
-			i = 0;
-			for( ConstraintPropertyAccess cpa : lastTrace.propertyAccesses) {
-				i++;
-				System.out.println("\nConstrain Property Access: " + i);
-				
-				System.out.println("CPA name: " + cpa.execution.constraint.getName()); 		// constraint name?
-				cpa.getExecution().getConstraint().getName(); 	// constraint name?
-				EObject elementAsEobject = (EObject) cpa.getModelElement();
-				System.out.println("element hashCode: "+ elementAsEobject.hashCode()); 	//
-				System.out.println("property name: " + cpa.getPropertyName()); 	//
-				cpa.getExecution().getConstraint().getMessageBlock();
-				
-			}
-		}
-	}
-	
-	public IncrementalEvlTrace  getLastTrace() {
-		//System.out.println("\ngetLastTrace(): " + lastTrace.propertyAccesses.toString());
-		return lastTrace;
-	}
-	
-	
-	@Override
-	public void notifyChanged(Notification notification) {
-		super.notifyChanged(notification);
-		notifications.add(notification);
-		
-		EStructuralFeature feature = (EStructuralFeature) notification.getFeature(); // unpack the feature from the notification
-				
-		System.out.println("\n [!] notifyChanged(Notification notification) : " +notification.getFeature().hashCode() + " " + notification.getOldValue() + " to " + notification.getNewValue() + "\n");
-		System.out.println("notification for feature name: " +  feature.getName() );
-		System.out.println("");
-	}
-	
-	public IncrementalEvlValidator getValidator() {
-		return validator;
-	}
-	
-	public boolean mustRevalidate(ResourceSet resourceSet) {
-		return !notifications.isEmpty();
-	}
-	
+    private boolean validationHasRun = false; //Track that we have run one Validation
+    private IncrementalEvlTrace lastTrace = null;
+    private Set<UnsatisfiedConstraint> unsatisfiedConstraints;
+
+    protected IncrementalEvlValidator validator = null;
+    protected List<Notification> notifications = new ArrayList<>();
+
+    public IncrementalEvlValidatorAdapter(IncrementalEvlValidator validator) {
+        this.validator = validator;
+    }
+
+    public void revalidate(ResourceSet resourceSet) throws Exception {
+        System.out.println("\n [!] IncrementalEvlValidatorAdapter.revalidate() called: " + resourceSet + "\n");
+
+
+        validate(resourceSet);
+
+        notifications.clear();
+
+    }
+
+    public void validate(ResourceSet resourceSet) throws Exception {
+        System.out.print("\n [!] IncrementalEvlValidatorAdapter.validate() called\n");
+
+        // Model (root element)
+        InMemoryEmfModel model = new InMemoryEmfModel(resourceSet.getResources().get(0));
+        model.setConcurrent(true);
+        System.out.println("Model name : '" + model.getName() + "' hashCode: " + model.hashCode());
+
+        // All Model elements
+        System.out.println("Model elements :");
+        Collection<EObject> elements = model.allContents();
+        for (EObject e : elements) {
+            System.out.println("hashCode: " + e.hashCode() + " object: " + e.toString());
+        }
+
+
+        // Module for doing evaluations
+        // Two ways to setup a module, with and without prior knowledge of the last validation
+        IncrementalEvlModule module = null;
+        if (validationHasRun) {
+            module = new IncrementalEvlModule(notifications, unsatisfiedConstraints, lastTrace);
+        } else {
+            module = new IncrementalEvlModule();
+        }
+        module.parse(validator.getConstraints());    // constraints ArrayList<E>[]
+        module.getContext().getModelRepository().addModel(model);
+
+
+        System.out.println("\nConstraints to Execute : ");
+        List<Constraint> constraintsToExecute = module.getConstraints();
+        int i = 0;
+        for (Constraint c : constraintsToExecute) {
+            i++;
+            System.out.println(i + ", " + c.getName());
+        }
+
+
+        //Set<UnsatisfiedConstraint> unsatisfiedConstraints = module.execute();
+        System.out.println("\n [!] ...Executing validation...\n");
+        unsatisfiedConstraints = module.execute();
+
+        validationHasRun = true; // Confirm at least one validation hasRun
+        lastTrace = module.getTrace();
+        System.out.println("\n [!] Review trace (constraintPropertyAccess Objects) in EvlModule:\n" + lastTrace.propertyAccesses.toString());
+        if (null != lastTrace) {
+            i = 0;
+            for (ConstraintPropertyAccess cpa : lastTrace.propertyAccesses) {
+                i++;
+                System.out.println("\nConstrain Property Access: " + i);
+
+                System.out.println("CPA name: " + cpa.execution.constraint.getName());        // constraint name?
+                cpa.getExecution().getConstraint().getName();    // constraint name?
+                EObject elementAsEobject = (EObject) cpa.getModelElement();
+                System.out.println("element hashCode: " + elementAsEobject.hashCode());    //
+                System.out.println("property name: " + cpa.getPropertyName());    //
+                cpa.getExecution().getConstraint().getMessageBlock();
+
+            }
+        }
+    }
+
+    public IncrementalEvlTrace getLastTrace() {
+        //System.out.println("\ngetLastTrace(): " + lastTrace.propertyAccesses.toString());
+        return lastTrace;
+    }
+
+
+    @Override
+    public void notifyChanged(Notification notification) {
+        super.notifyChanged(notification);
+        notifications.add(notification);
+
+        EStructuralFeature feature = (EStructuralFeature) notification.getFeature(); // unpack the feature from the notification
+
+        System.out.println("\n [!] notifyChanged(Notification notification) : " + notification.getFeature().hashCode() + " " + notification.getOldValue() + " to " + notification.getNewValue() + "\n");
+        System.out.println("notification for feature name: " + feature.getName());
+        System.out.println("");
+    }
+
+    public IncrementalEvlValidator getValidator() {
+        return validator;
+    }
+
+    public boolean mustRevalidate(ResourceSet resourceSet) {
+        return !notifications.isEmpty();
+    }
+
 }
