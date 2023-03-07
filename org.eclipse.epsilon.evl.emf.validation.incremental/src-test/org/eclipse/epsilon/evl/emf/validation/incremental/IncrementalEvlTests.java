921f89646c3e71d81f7967c8b6eed9e1cca82293
Antonio García-Domínguez <a.garcia-dominguez@york.ac.uk>
2023-03-07 14:36:27 +0000

    Add JUnit 5 and test source folder to project

---
+++ org.eclipse.epsilon.evl.emf.validation.incremental/src-test/org/eclipse/epsilon/evl/emf/validation/incremental/IncrementalEvlTests.java
@@ -0,0 +1,13 @@
+package org.eclipse.epsilon.evl.emf.validation.incremental;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class IncrementalEvlTests {
+
+    @Test
+    public void collectPropertyAccess() {
+        assertTrue(true);
+    }
+}
