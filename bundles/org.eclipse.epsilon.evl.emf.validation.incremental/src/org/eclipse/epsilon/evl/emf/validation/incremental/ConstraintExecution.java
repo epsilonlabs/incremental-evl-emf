ad75af0bce8ec1bd590ade4eb6f6a6e92bd6edcd
OwenR-York <owen.reynolds@york.ac.uk>
2023-05-05 15:04:33 +0100

    Rolled back a bad change to returning CPAs from Executions which have many accesses not one.

---
+++ bundles/org.eclipse.epsilon.evl.emf.validation.incremental/src/org/eclipse/epsilon/evl/emf/validation/incremental/ConstraintExecution.java
@@ -0,0 +1,14 @@
+package org.eclipse.epsilon.evl.emf.validation.incremental;
+
+import java.util.List;
+
+import org.eclipse.epsilon.common.module.ModuleElement;
+
+public class ConstraintExecution extends Execution{
+
+	public ConstraintExecution(ModuleElement constraint, Object self) {
+		super(constraint, self);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
