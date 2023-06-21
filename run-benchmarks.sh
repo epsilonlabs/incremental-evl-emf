31c40864eb46cf692524fcac07ce8caec34366d0
Antonio García-Domínguez <a.garcia-dominguez@york.ac.uk>
2023-06-21 19:14:01 +0100

    Implement benchmark for single validation (without changes)

--- run-benchmarks.sh
+++ run-benchmarks.sh
@@ -2,5 +2,10 @@
 
 set -e
 
+EVL_PATH=tests/org.eclipse.epsilon.evl.emf.validation.incremental.benchmarks/src/main/resources/constraints.evl
+MODEL_PATH=examples/org.eclipse.epsilon.examples.metamodels.components.model.generated/generated.ccl
+
 mvn -f pom-plain.xml clean verify
-java -jar tests/org.eclipse.epsilon.evl.emf.validation.incremental.benchmarks/target/benchmarks.jar
+java -jar tests/org.eclipse.epsilon.evl.emf.validation.incremental.benchmarks/target/benchmarks.jar \
+	-p "constraintsPath=$EVL_PATH" \
+	-p "modelPath=$MODEL_PATH"
